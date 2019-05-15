4bbd632f1c5522086482f071c59201d22a5145d7
Erik Peterson <erik.peterson@ttu.edu>
2019-05-15 10:45:12 -0500

    Saving text with unnecessary stuff stripped out and div formatting classes retained.

--- src/peterson/erik/updatedasv/CreateHTMLFiles.java
+++ src/peterson/erik/updatedasv/CreateHTMLFiles.java
@@ -3,10 +3,10 @@ package peterson.erik.updatedasv;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,75 +15,77 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /* package */ class CreateHTMLFiles {
-    private static final Map<String, String> FILE_NAME_MAP = new HashMap<>();
+
+    // Converts old file names (abbreviation) to book names
+    private static final Map<String, String> BOOK_NAME_MAP = new HashMap<>();
 
     static {
-        FILE_NAME_MAP.put("GEN", "Genesis");
-        FILE_NAME_MAP.put("EXO", "Exodus");
-        FILE_NAME_MAP.put("LEV", "Leviticus");
-        FILE_NAME_MAP.put("NUM", "Numbers");
-        FILE_NAME_MAP.put("DEU", "Deuteronomy");
-        FILE_NAME_MAP.put("JOS", "Joshua");
-        FILE_NAME_MAP.put("JDG", "Judges");
-        FILE_NAME_MAP.put("RUT", "Ruth");
-        FILE_NAME_MAP.put("1SA", "1 Samuel");
-        FILE_NAME_MAP.put("2SA", "2 Samuel");
-        FILE_NAME_MAP.put("1KI", "1 Kings");
-        FILE_NAME_MAP.put("2KI", "2 Kings");
-        FILE_NAME_MAP.put("1CH", "1 Chronicles");
-        FILE_NAME_MAP.put("2CH", "2 Chronicles");
-        FILE_NAME_MAP.put("EZR", "Ezra");
-        FILE_NAME_MAP.put("NEH", "Nehemiah");
-        FILE_NAME_MAP.put("EST", "Esther");
-        FILE_NAME_MAP.put("JOB", "Job");
-        FILE_NAME_MAP.put("PSA", "Psalms");
-        FILE_NAME_MAP.put("PRO", "Proverbs");
-        FILE_NAME_MAP.put("ECC", "Ecclesiastes");
-        FILE_NAME_MAP.put("SNG", "Song of Solomon");
-        FILE_NAME_MAP.put("ISA", "Isaiah");
-        FILE_NAME_MAP.put("JER", "Jeremiah");
-        FILE_NAME_MAP.put("LAM", "Lamentations");
-        FILE_NAME_MAP.put("EZK", "Ezekiel");
-        FILE_NAME_MAP.put("DAN", "Daniel");
-        FILE_NAME_MAP.put("HOS", "Hosea");
-        FILE_NAME_MAP.put("JOL", "Joel");
-        FILE_NAME_MAP.put("AMO", "Amos");
-        FILE_NAME_MAP.put("OBA", "Obadiah");
-        FILE_NAME_MAP.put("JON", "Jonah");
-        FILE_NAME_MAP.put("MIC", "Micah");
-        FILE_NAME_MAP.put("NAM", "Nahum");
-        FILE_NAME_MAP.put("HAB", "Habakkuk");
-        FILE_NAME_MAP.put("ZEP", "Zephaniah");
-        FILE_NAME_MAP.put("HAG", "Haggai");
-        FILE_NAME_MAP.put("ZEC", "Zechariah");
-        FILE_NAME_MAP.put("MAL", "Malachi");
-        FILE_NAME_MAP.put("MAT", "Matthew");
-        FILE_NAME_MAP.put("MRK", "Mark");
-        FILE_NAME_MAP.put("LUK", "Luke");
-        FILE_NAME_MAP.put("JHN", "John");
-        FILE_NAME_MAP.put("ACT", "Acts");
-        FILE_NAME_MAP.put("ROM", "Romans");
-        FILE_NAME_MAP.put("1CO", "1 Corinthians");
-        FILE_NAME_MAP.put("2CO", "2 Corinthians");
-        FILE_NAME_MAP.put("GAL", "Galatians");
-        FILE_NAME_MAP.put("EPH", "Ephesians");
-        FILE_NAME_MAP.put("PHP", "Philippians");
-        FILE_NAME_MAP.put("COL", "Colossians");
-        FILE_NAME_MAP.put("1TH", "1 Thessalonians");
-        FILE_NAME_MAP.put("2TH", "2 Thessalonians");
-        FILE_NAME_MAP.put("1TI", "1 Timothy");
-        FILE_NAME_MAP.put("2TI", "2 Timothy");
-        FILE_NAME_MAP.put("TIT", "Titus");
-        FILE_NAME_MAP.put("PHM", "Philemon");
-        FILE_NAME_MAP.put("HEB", "Hebrews");
-        FILE_NAME_MAP.put("JAS", "James");
-        FILE_NAME_MAP.put("1PE", "1 Peter");
-        FILE_NAME_MAP.put("2PE", "2 Peter");
-        FILE_NAME_MAP.put("1JN", "1 John");
-        FILE_NAME_MAP.put("2JN", "2 John");
-        FILE_NAME_MAP.put("3JN", "3 John");
-        FILE_NAME_MAP.put("JUD", "Jude");
-        FILE_NAME_MAP.put("REV", "Revelation");
+        /*BOOK_NAME_MAP.put("GEN", "Genesis");
+        BOOK_NAME_MAP.put("EXO", "Exodus");
+        BOOK_NAME_MAP.put("LEV", "Leviticus");
+        BOOK_NAME_MAP.put("NUM", "Numbers");
+        BOOK_NAME_MAP.put("DEU", "Deuteronomy");
+        BOOK_NAME_MAP.put("JOS", "Joshua");
+        BOOK_NAME_MAP.put("JDG", "Judges");
+        BOOK_NAME_MAP.put("RUT", "Ruth");
+        BOOK_NAME_MAP.put("1SA", "1 Samuel");
+        BOOK_NAME_MAP.put("2SA", "2 Samuel");
+        BOOK_NAME_MAP.put("1KI", "1 Kings");
+        BOOK_NAME_MAP.put("2KI", "2 Kings");
+        BOOK_NAME_MAP.put("1CH", "1 Chronicles");
+        BOOK_NAME_MAP.put("2CH", "2 Chronicles");
+        BOOK_NAME_MAP.put("EZR", "Ezra");
+        BOOK_NAME_MAP.put("NEH", "Nehemiah");
+        BOOK_NAME_MAP.put("EST", "Esther");
+        BOOK_NAME_MAP.put("JOB", "Job");
+        BOOK_NAME_MAP.put("PSA", "Psalms");
+        BOOK_NAME_MAP.put("PRO", "Proverbs");
+        BOOK_NAME_MAP.put("ECC", "Ecclesiastes");
+        BOOK_NAME_MAP.put("SNG", "Song of Solomon");
+        BOOK_NAME_MAP.put("ISA", "Isaiah");
+        BOOK_NAME_MAP.put("JER", "Jeremiah");
+        BOOK_NAME_MAP.put("LAM", "Lamentations");
+        BOOK_NAME_MAP.put("EZK", "Ezekiel");
+        BOOK_NAME_MAP.put("DAN", "Daniel");
+        BOOK_NAME_MAP.put("HOS", "Hosea");
+        BOOK_NAME_MAP.put("JOL", "Joel");
+        BOOK_NAME_MAP.put("AMO", "Amos");
+        BOOK_NAME_MAP.put("OBA", "Obadiah");
+        BOOK_NAME_MAP.put("JON", "Jonah");
+        BOOK_NAME_MAP.put("MIC", "Micah");
+        BOOK_NAME_MAP.put("NAM", "Nahum");
+        BOOK_NAME_MAP.put("HAB", "Habakkuk");
+        BOOK_NAME_MAP.put("ZEP", "Zephaniah");
+        BOOK_NAME_MAP.put("HAG", "Haggai");
+        BOOK_NAME_MAP.put("ZEC", "Zechariah");
+        BOOK_NAME_MAP.put("MAL", "Malachi");
+        BOOK_NAME_MAP.put("MAT", "Matthew");*/
+        BOOK_NAME_MAP.put("MRK", "Mark");
+        BOOK_NAME_MAP.put("LUK", "Luke");
+        BOOK_NAME_MAP.put("JHN", "John");
+        BOOK_NAME_MAP.put("ACT", "Acts");
+        BOOK_NAME_MAP.put("ROM", "Romans");
+        BOOK_NAME_MAP.put("1CO", "1 Corinthians");
+        BOOK_NAME_MAP.put("2CO", "2 Corinthians");
+        BOOK_NAME_MAP.put("GAL", "Galatians");
+        BOOK_NAME_MAP.put("EPH", "Ephesians");
+        BOOK_NAME_MAP.put("PHP", "Philippians");
+        BOOK_NAME_MAP.put("COL", "Colossians");
+        BOOK_NAME_MAP.put("1TH", "1 Thessalonians");
+        BOOK_NAME_MAP.put("2TH", "2 Thessalonians");
+        BOOK_NAME_MAP.put("1TI", "1 Timothy");
+        BOOK_NAME_MAP.put("2TI", "2 Timothy");
+        BOOK_NAME_MAP.put("TIT", "Titus");
+        BOOK_NAME_MAP.put("PHM", "Philemon");
+        BOOK_NAME_MAP.put("HEB", "Hebrews");
+        BOOK_NAME_MAP.put("JAS", "James");
+        BOOK_NAME_MAP.put("1PE", "1 Peter");
+        BOOK_NAME_MAP.put("2PE", "2 Peter");
+        BOOK_NAME_MAP.put("1JN", "1 John");
+        BOOK_NAME_MAP.put("2JN", "2 John");
+        BOOK_NAME_MAP.put("3JN", "3 John");
+        BOOK_NAME_MAP.put("JUD", "Jude");
+        BOOK_NAME_MAP.put("REV", "Revelation");
     }
 
     // This matches only chapter files, e.g. GEN01.htm or 1CH01.htm
@@ -91,8 +93,15 @@ import java.util.regex.Pattern;
 
 
     /* package */ static void createFiles() throws IOException {
-        Set<String> originalFileKeys = FILE_NAME_MAP.keySet();
-        File[] files = new File("originalText").listFiles();
+
+        // Make sure we have a place to go...
+        File updatedFilesBaseDirectory = new File(Util.UPDATED_TEXT_DIR);
+        if ( !updatedFilesBaseDirectory.exists()) {
+            updatedFilesBaseDirectory.mkdir();
+        }
+
+        Set<String> originalFileKeys = BOOK_NAME_MAP.keySet();
+        File[] files = new File(Util.ORIGINAL_TEXT_DIR).listFiles();
         for ( File file : files) {
             String originalFilename = file.getName();
             Matcher chapterMatcher = CHAPTER_PATTERN.matcher(originalFilename);
@@ -102,22 +111,58 @@ import java.util.regex.Pattern;
             }
             for ( String key : originalFileKeys) {
                 if ( originalFilename.startsWith(key)) {
-                    System.out.println("Found " + FILE_NAME_MAP.get(key) + " " + chapterMatcher.group(1));
+//                    System.out.println("Found " + BOOK_NAME_MAP.get(key) + " " + chapterMatcher.group(1));
+
                     Document originalDoc = Jsoup.parse(file, "UTF-8", "http://example.com/");
                     StringBuilder newText = new StringBuilder();
                     newText.append("<html><body>");
-                    Elements paragraphElements = originalDoc.getElementsByClass("p");
-                    for ( Element paragraph : paragraphElements) {
+                    Elements divElements = originalDoc.getElementsByTag("div");
+                    for ( Element paragraph : divElements) {
+                        String classname = paragraph.className();
+                        switch ( classname) {
+                            // We want
+                            //   'p' (paragraph),
+                            //   'q' (quote),
+                            //   'm' (beginning of quote?)
+                            //   'mt' (beginning of book)
+                            //   'b' (break)
+                            //   'nb' (no break?)
+                            //   'pi' (paragraph indented?)
+                            case "p":
+                            case "q":
+                            case "m":
+                            case "mt":
+                            case "b":
+                            case "nb":
+                            case "pi":
+                                break;
+                            // We don't need these
+                            case "main":
+                            case "chapterlabel":
+                            case "footnote":
+                            case "copyright":
+                                continue;
+                            default:
+                                System.out.println("Another kind of div class: " + classname);
+                                continue;
+                        }
+
                         String text = paragraph.html();
                         text = text.replaceAll("\\n", "");
-                        newText.append("<p>").append(text).append("<\\p>");
+                        newText.append("<div class=\"" + classname + "\" >").append(text).append("</div>");
                     }
-                    newText.append("<\\body><\\html>");
+                    newText.append("</body></html>");
 //                    System.out.println("Now have " + newText.toString());
 
-                    File newFile = new File("updateText/" + FILE_NAME_MAP.get(key) + " " + chapterMatcher.group(1));
+                    File newBookDirectory = new File(updatedFilesBaseDirectory, BOOK_NAME_MAP.get(key));
+                    if ( !newBookDirectory.exists()) {
+                        newBookDirectory.mkdir();
+                    }
 
-
+                    File newFile = new File(newBookDirectory, BOOK_NAME_MAP.get(key) + chapterMatcher.group(1) + ".htm");
+                    FileWriter fileWriter = new FileWriter(newFile);
+                    fileWriter.append(newText.toString());
+                    fileWriter.close();
                     break;
                 }
             }
---
+++ src/peterson/erik/updatedasv/Util.java
@@ -0,0 +1,6 @@
+package peterson.erik.updatedasv;
+
+public class Util {
+    public static final String ORIGINAL_TEXT_DIR = "originalText";
+    public static final String UPDATED_TEXT_DIR = "updatedText";
+}
